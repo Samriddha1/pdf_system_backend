b16c7ceae480c2629e01cf1c01e5ef62734e88d7
samriddha <tuyaroy50@gmail.com>
2025-04-22 01:03:34 +0530

    commit

--- src/main/java/com/pdf/PdfApplication.java
+++ src/main/java/com/pdf/PdfApplication.java
@@ -2,13 +2,10 @@ package com.pdf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class PdfApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PdfApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PdfApplication.class, args);
+    }
 }
